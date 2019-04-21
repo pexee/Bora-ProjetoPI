14b3888d401e96a78de788ac6b80cc032d944c53
vnc10 <viniciusbosa@gmail.com>
2019-04-21 19:00:32 -0300

    tela de cadastro de usuario adicionada

--- src/components/cadastroUsuario.js
+++ src/components/cadastroUsuario.js
@@ -6,8 +6,22 @@ import {StyleSheet,
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Input,
+  Button,
+  ThemeProvider,
+  Header,
 } from 'react-native-elements';
 
+const theme = {
+  colors: {
+    primary: 'white'
+  }
+}
+
+const theme2 = {
+  colors: {
+    primary: 'white'
+  }
+}
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -21,32 +35,36 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.containerPrincipal}>
+      <Header centerComponent={{ text: 'Cadastro', style: { fontSize: 25, color: '#fff' } } }/>
         <View style={styles.containerInput}>
           <View style={styles.input}>
             <Input placeholderTextColor='#fff' placeholder='Nome' leftIcon={
-            <Icon name='user' size={24} color='white'/>}
-            />
+            <Icon name='user' size={24} color='white'/>}/>
           </View>
           <View style={styles.input}>
             <Input placeholderTextColor='#fff' placeholder='E-mail' leftIcon={
-            <Icon name='at' size={24} color='white'/>}
-            />
+            <Icon name='at' size={24} color='white'/>}/>
           </View>
           <View style={styles.input}>
             <Input underLineColorAndroid={'transparent'} placeholderTextColor='white' placeholder='Senha' leftIcon={
-            <Icon name='lock' size={24} color='white'/>}
-            />
+            <Icon name='lock' size={24} color='white'/>}/>
           </View>
           <View style={styles.input}>
             <Input underLineColorAndroid={'transparent'} placeholderTextColor='white' placeholder='Digite sua senha novamente' leftIcon={
-            <Icon name='lock' size={24} color='white'/>}
-            />
+            <Icon name='lock' size={24} color='white'/>}/>
+          </View>
+          <View style={styles.button}>
+            <ThemeProvider theme={theme}>
+              <Button raised title='Confirmar' titleStyle={{ color: 'black' }}/>
+            </ThemeProvider>
+          </View>
+          <View style={styles.button}>
+            <ThemeProvider theme={theme2}>
+              <Button raised title='Cancelar' titleStyle={{ color: 'black' }}/>
+            </ThemeProvider>
           </View>
         </View>
       </View>
-
-
-
     );
   }
 }
@@ -57,7 +75,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#1e90ff',
   },
   containerInput: {
-    marginTop: 150,
+    marginTop: 60,
     paddingLeft: 20,
     paddingRight: 20,
   },
@@ -65,5 +83,10 @@ const styles = StyleSheet.create({
     marginTop: 10,
     borderRadius:15,
     backgroundColor: '#00bfff',
+  },
+  button: {
+    marginTop: 10,
+    paddingLeft: 70,
+    paddingRight: 70,
   },
 });
