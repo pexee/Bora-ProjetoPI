74cd49421a1cd381b1a912fbe067126cb3e5fb7a
vnc10 <viniciusbosa@gmail.com>
2019-04-28 16:12:22 -0300

    tela de editar usuario adicionada

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { View, Button, Text } from 'react-native';
 import CadastroEventoPage1 from './Evento/cadastroEventoPage1'
+import EditarUsuario from './editarUsuario'
 import {createStackNavigator, createAppContainer} from 'react-navigation';
 
 export default class App extends Component {
@@ -12,10 +13,15 @@ export default class App extends Component {
 class paginaPrincipal extends Component{
     render() {
       return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', marginTop: 10 }}>
         <Button 
           title="Cadastrar Evento" onPress={() => this.props.navigation.navigate('cadastroEventoPage1') }
         />
+        <View  style={{marginTop: 10}}>
+        <Button
+          title="Editar usuario" onPress={() => this.props.navigation.navigate('editarUsuario') }
+        />
+        </View>
       </View>
     );
     }
@@ -29,11 +35,20 @@ class cadastroEventoPage1 extends Component {
     }
 }
 
+class editarUsuario extends Component {
+  render(){
+      return (
+          <EditarUsuario/>
+      );
+  }
+}
+
 
 const AppSwitchNavigator = createStackNavigator({
     paginaPrincipal: {screen: paginaPrincipal,     navigationOptions: {
       header: null,
     }, },
+    editarUsuario: {screen: editarUsuario },
     cadastroEventoPage1: {screen: cadastroEventoPage1}
   });
   
@@ -41,6 +56,13 @@ const AppContainer = createAppContainer(AppSwitchNavigator);
 
 cadastroEventoPage1.navigationOptions = {
   title: 'Criar Evento',
+  headerTintColor: "white",
+  headerStyle: {
+    backgroundColor:'#1e90ff'
+  }  
+}
+editarUsuario.navigationOptions = {
+  title: 'Editar Usuario',
   headerTintColor: "white",
   headerStyle: {
     backgroundColor:'#1e90ff'
--- src/components/editarUsuario.js
+++ src/components/editarUsuario.js
@@ -13,16 +13,12 @@ export default class App extends Component{
   
   render() {
     return (
-      <View style={styles.containerPrincipal}>
+        <View style={styles.containerPrincipal}>
         <View style={styles.containerInput}>
           <ScrollView>
             <View style={styles.input}>
               <Input placeholderTextColor='#fff' placeholder='Nome' leftIcon={
               <Icon name='user' size={24} color='white'/>}/>
-            </View>
-            <View style={styles.input}>
-              <Input placeholderTextColor='#fff' placeholder='E-mail' leftIcon={
-              <Icon name='at' size={24} color='white'/>}/>
             </View>
             <View style={styles.input}>
               <Input underLineColorAndroid={'transparent'} placeholderTextColor='white' placeholder='Senha' secureTextEntry={true} leftIcon={
@@ -40,28 +36,28 @@ export default class App extends Component{
           </View>
         </View>
       </View>
-    );
+  );
   }
 }
 
 const styles = StyleSheet.create({
-  containerPrincipal: {
-    flex: 1,
-    backgroundColor: '#1e90ff',
-  },
-  containerInput: {
-    marginTop: 60,
-    paddingLeft: 20,
-    paddingRight: 20,
-  },
-  input: {
-    marginTop: 10,
-    borderRadius:15,
-    backgroundColor: '#00bfff',
-  },
-  button: {
-    marginTop: 10,
-    paddingLeft: 70,
-    paddingRight: 70,
-  },
-});
+    containerPrincipal: {
+      flex: 1,
+      backgroundColor: '#1e90ff',
+    },
+    containerInput: {
+      marginTop: 60,
+      paddingLeft: 20,
+      paddingRight: 20,
+    },
+    input: {
+      marginTop: 10,
+      borderRadius:15,
+      backgroundColor: '#00bfff',
+    },
+    button: {
+      marginTop: 10,
+      paddingLeft: 70,
+      paddingRight: 70,
+    },
+  });
