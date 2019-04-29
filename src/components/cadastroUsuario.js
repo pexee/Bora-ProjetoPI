d17ad1f6af95f07b52369c2525f2163be55dfdc7
geovani <geeopedroso@gmail.com>
2019-04-29 09:32:20 -0300

    criacao de usuario email/senha .closes#17

--- src/components/cadastroUsuario.js
+++ src/components/cadastroUsuario.js
@@ -3,6 +3,11 @@ import {StyleSheet, ScrollView, View} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Input, Button, ThemeProvider} from 'react-native-elements';
 
+
+import firebase from 'react-native-firebase';
+
+
+
 const theme = {
   colors: {
     primary: 'white'
@@ -10,32 +15,86 @@ const theme = {
 }
 
 export default class App extends Component{
+
+  state = {
+    nome: 'geovani',
+    email: 'geovanipedroso01@hotmail.com',
+    password: '',
+    password2: '',
+    isAuthenticated: false,
+  };
+
+  criarconta = async () => {
+    const { nome, email, password } = this.state;
+    var database = firebase.database();
+
+     
+    try {
+      if(this.state.password.length < 6){
+        alert('senha deve conter 6 ou mais caracteres')
+        return;
+      }
+      
+      if(this.state.password.localeCompare(this.state.password2)){
+        alert('senhas diferentes')
+        return;
+
+      }
+      
+
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(function (user) {
+        firebase.database().ref('/usuarios/'+nome).set(user);
+
+      })
+    
+    this.setState({ isAuthenticated: true });
+      
+      
+    } catch (error) {
+      
+    }
+    
+  }
   
   render() {
     return (
       <View style={styles.containerPrincipal}>
+        {this.state.isAuthenticated ? alert( 'Criado com sucesso'): null }
         <View style={styles.containerInput}>
           <ScrollView>
             <View style={styles.input}>
               <Input placeholderTextColor='#fff' placeholder='Nome' leftIcon={
-              <Icon name='user' size={24} color='white'/>}/>
+              <Icon name='user' size={24} color='white'/>}
+              value={this.state.nome}
+              onChangeText={nome => this.setState({ nome })}
+              />
             </View>
             <View style={styles.input}>
               <Input placeholderTextColor='#fff' placeholder='E-mail' leftIcon={
-              <Icon name='at' size={24} color='white'/>}/>
+              <Icon name='at' size={24} color='white'/>}
+              value={this.state.email}
+              onChangeText={email => this.setState({ email })}
+              />
             </View>
             <View style={styles.input}>
               <Input underLineColorAndroid={'transparent'} placeholderTextColor='white' placeholder='Senha' secureTextEntry={true} leftIcon={
-              <Icon name='lock' size={24} color='white'/>}/>
+              <Icon name='lock' size={24} color='white'/>}
+              value={this.state.password}
+              onChangeText={password => this.setState({ password })}
+              />
             </View>
             <View style={styles.input}>
               <Input underLineColorAndroid={'transparent'} placeholderTextColor='white' placeholder='Digite sua senha novamente' secureTextEntry={true} leftIcon={
-              <Icon name='lock' size={24} color='white'/>}/>
+              <Icon name='lock' size={24} color='white'/>}
+              value={this.state.password2}
+              onChangeText={password2 => this.setState({ password2 })}
+              />
             </View>
           </ScrollView>
           <View style={styles.button}>
             <ThemeProvider theme={theme}>
-              <Button raised title='Confirmar' titleStyle={{ color: 'black' }}/>
+              <Button raised title='Confirmar' onPress={this.criarconta} titleStyle={{ color: 'black' }}/>
             </ThemeProvider>
           </View>
         </View>
